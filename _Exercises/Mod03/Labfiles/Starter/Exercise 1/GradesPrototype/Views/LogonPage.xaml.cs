e7a21caf31a67ac7684cf71c6e8f779740fabd92
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-08-07 11:42:49 +0200

    feat: C# module 3

---
+++ _Exercises/Mod03/Labfiles/Starter/Exercise 1/GradesPrototype/Views/LogonPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Views
+{
+    /// <summary>
+    /// Interaction logic for LogonPage.xaml
+    /// </summary>
+    public partial class LogonPage : UserControl
+    {
+        public LogonPage()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Members
+        // Exercise 1: Task 2a: Define the LogonSuccess event handler
+
+        public event EventHandler LogonSuccess;
+
+        #endregion
+
+        #region Logon Validation
+
+        // Exercise 1: Task 2b: Implement the Logon_Click event handler for the Logon button
+        // Simulate logging on (no validation or authentication performed yet)
+
+        private void Logon_Click(object sender, RoutedEventArgs e)
+        {
+            SessionContext.UserName = username.Text;
+            SessionContext.UserRole = userrole.IsChecked == true ? Role.Teacher : Role.Student;
+
+            if (userrole.IsChecked == false) // Student
+                SessionContext.CurrentStudent = "Eric Gruber";
+
+            LogonSuccess?.Invoke(this, e);
+
+        }
+
+        #endregion
+    }
+}
---
+++ _Exercises/Mod03/Labfiles/Starter/Exercise 3/GradesPrototype/Views/LogonPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Views
+{
+    /// <summary>
+    /// Interaction logic for LogonPage.xaml
+    /// </summary>
+    public partial class LogonPage : UserControl
+    {
+        public LogonPage()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Members
+        public event EventHandler LogonSuccess;
+        public event EventHandler LogonFailed;
+
+        // Exercise 3: Task 1a: Define LogonFailed event
+
+        #endregion
+
+        #region Logon Validation
+
+        // Exercise 3: Task 1b: Validate the username and password against the Users collection in the MainWindow window
+        private void Logon_Click(object sender, RoutedEventArgs e)
+        {
+            var teacherQuery = from Teacher t in DataSource.Teachers
+                          where t.UserName == username.Text
+                          && t.Password == password.Password
+                          select t;
+
+            if (teacherQuery.Count() > 0) // Teacher
+            {
+                Teacher t = teacherQuery.First();
+                SessionContext.UserID = t.TeacherID;
+                SessionContext.UserRole = Role.Teacher;
+                SessionContext.UserName = t.UserName;
+                SessionContext.CurrentTeacher = t;
+
+                LogonSuccess?.Invoke(this, null);
+            }
+            else // Student or Invalid Login
+            {
+                var studentQuery = from Student s in DataSource.Students
+                                   where s.UserName == username.Text
+                                   && s.Password == password.Password
+                                   select s;
+
+                if (studentQuery.Count() > 0) // Student
+                {
+                    Student s = studentQuery.First();
+                    SessionContext.UserID = s.TeacherID;
+                    SessionContext.UserRole = Role.Student;
+                    SessionContext.UserName = s.UserName;
+                    SessionContext.CurrentStudent = s;
+
+                    LogonSuccess?.Invoke(this, null);
+                }
+                else // Invalid Login
+                {
+                    LogonFailed?.Invoke(this, null);
+                }
+            }
+        }
+        #endregion
+    }
+}
